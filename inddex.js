3a5e461d1337cf5b7c97d535c4eaa989f74277ba
tshims <bentshims21@gmail.com>
2025-02-01 06:11:50 +0100

    configuration du son  `do` lors que la touche `d` est pressé et relaché

---
+++ inddex.js
@@ -0,0 +1,30 @@
+const btn_do = document.querySelector(`#btn1`);
+const btn_re = document.querySelector(`#btn2`);
+const btn_mi = document.querySelector(`#btn3`);
+const btn_fa = document.querySelector(`#btn4`);
+const btn_sol = document.querySelector(`#btn5`);
+const btn_la = document.querySelector(`#btn6`);
+const btn_si = document.querySelector(`#btn7`);
+const btn_do2 = document.querySelector(`#btn8`);
+const btn_re2 = document.querySelector(`#btn9`);
+const btn_la2 = document.querySelector(`#btn10`);
+const btn_sol2 = document.querySelector(`#btn11`);
+const btn_fa2 = document.querySelector(`#btn12`);
+const music_do = document.querySelector(`#do`);
+console.log(music_do);
+document.addEventListener(`keydown`, (e) => {
+  if (e.key.toLowerCase() === `d`) {
+    console.log(`press !`);
+    btn_do.style.backgroundColor = `orange`;
+    music_do.play();
+    music_do.currentTime = 0;
+  }
+});
+
+document.addEventListener(`keyup`, (e) => {
+  if (e.key.toLocaleLowerCase() === `d`) {
+    console.log(`quit`);
+    music_do.pause();
+    btn_do.style.backgroundColor = `#f0f0f0`;
+  }
+});
